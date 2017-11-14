ee47139db2abd6b6cff83365e7e9480ca9a326ce
Francois <francois@n-dream.com>
2017-11-14 09:33:24 +0100

    Minified changes

--- build/js/screen_1.0.0.js
+++ build/js/screen_1.0.0.js
@@ -1635,6 +1635,32 @@ AirApp.controllers.controller('MainCtrl',
   };
 
 }]);
+/**
+ * This is an example directive. Use it to kickstart a new directive.
+ * Happy Coding. Please remove me before releasing.
+*/
+AirApp.directives.directive("exampleDirective", [function() {
+  return {
+    // E = element, A = attribute, C = class, M = comment
+    restrict: 'EA',
+    templateUrl: function(elem, attrs) {
+      return attrs.templateUrl || 'views/controller/directives/_example_directive.html';
+    },
+    scope: {
+      // @ reads the attribute value, = provides two-way binding, & works with functions
+      items: '=items',
+      key: '@',
+    },
+    // Embed a custom controller in the directive
+    controller: function($scope) {
+
+    },
+    // DOM manipulation
+    link: function ($scope, element, attrs) {
+
+    }
+  };
+}]);
  AirApp.services.factory('GameService', ['$http', '$q', 'PlayerService', 'AirConsoleService',
     function ($http, $q, PlayerService, AirConsoleService) {
 
