fb0ce3e6e7b8ba51d859e0ebd0b07a00e326fac2
Francois <francois@n-dream.com>
2018-01-08 10:22:34 +0100

    Removed unused TrackPropertyService

--- build/js/screen_1.0.0.js
+++ build/js/screen_1.0.0.js
@@ -1541,41 +1541,6 @@ AirApp.services.factory('SoundService', [function () {
   };
 
 }]);
-AirApp.services.factory('TrackPropertyService', ['AirConsoleService',
-    function (AirConsoleService) {
-
-  var airconsole = null;
-  var service = {
-    properties: {}
-  };
-
-  service.init = function() {
-    airconsole = AirConsoleService.instance();
-    airconsole.on(AirConsoleService.Event.DeviceStateChange, function(device_id, data) {
-      for (var key in data) {
-        if (this.properties[key]) {
-          for (var i = 0; i < this.properties[key].length; i++) {
-            this.properties[key][i](device_id, data[key]);
-          }
-        }
-      }
-    });
-  };
-
-  service.set = function(prop, value) {
-    airconsole.setCustomDeviceStateProperty(prop, value);
-  };
-
-  service.track = function(prop, cb) {
-    if (!this.properties[prop]) {
-      this.properties[prop] = [];
-    }
-    this.properties[prop].push(cb);
-  };
-
-  return service;
-
-}]);
 AirApp.services.factory('ViewService', ['AirConsoleService', function (AirConsoleService) {
   var service = {
     ctrl: {},
